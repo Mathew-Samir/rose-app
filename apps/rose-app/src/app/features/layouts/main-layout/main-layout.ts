c520d126650ea30aabe4f4021240550b856d676a
Zinab Hashem <zadel@informatique@gmail.com>
2026-06-15 22:59:21 +0300

    footer

--- apps/rose-app/src/app/features/layouts/main-layout/main-layout.ts
+++ apps/rose-app/src/app/features/layouts/main-layout/main-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { SubNavbar } from "./sub-navbar/sub-navbar";
 import { Navbar } from "./navbar/navbar";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: "app-main-layout",
-  imports: [Navbar, SubNavbar],
+  imports: [Navbar, SubNavbar, Footer],
   templateUrl: "./main-layout.html"
 })
 export class MainLayout {}
