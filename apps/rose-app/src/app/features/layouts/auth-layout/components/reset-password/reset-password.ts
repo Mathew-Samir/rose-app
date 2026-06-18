4088f75487355670cfaafb7720afaa1095692966
Mathew-Samir <mthwsamir@gmail.com>
2026-06-18 23:48:18 +0300

    feat: 🎸 handle flow of reset password

--- apps/rose-app/src/app/features/layouts/auth-layout/components/reset-password/reset-password.ts
+++ apps/rose-app/src/app/features/layouts/auth-layout/components/reset-password/reset-password.ts
@@ -1,9 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+import { ForgetPassword } from "./components/forget-password/forget-password";
+import { ResetLinkReceived } from "./components/reset-link-received/reset-link-received";
 
 @Component({
   selector: "app-reset-password",
-  imports: [],
+  imports: [ForgetPassword, ResetLinkReceived],
   templateUrl: "./reset-password.html",
   styleUrl: "./reset-password.scss",
 })
-export class ResetPassword {}
+export class ResetPassword {
+  isEmailSent = signal(false);
+  sentEmail = signal("");
+
+  onEmailSent(email: string) {
+    this.sentEmail.set(email);
+    this.isEmailSent.set(true);
+  }
+}
